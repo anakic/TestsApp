9c06e33de454bb291ff1667b89cf658748b5234f
Antonio Nakic Alfirevic <antonio.nakic@gmail.com>
2017-04-08 19:39:00 +0200

    Preparing login.service on the client side

--- Jogging/ClientApp/app/login.service.ts
+++ Jogging/ClientApp/app/login.service.ts
@@ -1,29 +1,68 @@
 ﻿import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Http } from '@angular/http';
-import { Observable } from 'rxjs'
+import { Observable } from 'rxjs/Rx'
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class LoginService {
 
+    public loginUrl: string;
     public redirectUrl: string;
 
     public user: User;
 
-    public initUserFromServer() {
-        this.setUserAndRedirect({ id: 1, firstName: 'Test', lastName: 'User', email: 'test@user.com', canCrudAllEntries: false, canCrudUsers: false });
+    public initUserFromServer(): Observable<User> {
+        return this.http
+            .get(`/api/Account`, { withCredentials: true })
+            .map(result => {
+                return this.setUserAndRedirect(result.ok ? result.json() as User : null);
+            });
     }
 
-    private setUserAndRedirect(user: User) {
+    public signout(): Observable<boolean> {
+        return this.http.post(`/api/Account/Signout`, {})
+            .map(result => {
+                if (result.ok) {
+                    this.setUserAndRedirect(null);
+                }
+                return result.ok;
+            });
+    }
+
+    public login(userName: string, password: string): Observable<LoginResult> {
+        return this.http
+            .post(`/api/Account`, { userName, password })
+            .map(result => {
+                if (result.ok) {
+                    var user = this.setUserAndRedirect(result.ok ? result.json() as User : null);
+                    return { success: true, message: null, user: user };
+                }
+                else {
+                    return { success: true, message: result.statusText, user: null };
+                }
+            }, () => {
+                this.setUserAndRedirect(null); return { sucess: false, message: `Invalid credentials` }
+            });
+    }
+
+    private setUserAndRedirect(user: User): User {
         this.user = user;
-        if (user)
-            this.router.navigateByUrl(this.redirectUrl);
+        this.router.navigateByUrl(user ? this.redirectUrl : this.loginUrl);
+        return this.user;
     }
 
     constructor(private http: Http, private router: Router) {
+        this.loginUrl = '/login';
         this.redirectUrl = '/entries';
         this.initUserFromServer();
     }
+}
+
+interface LoginResult {
+    success: boolean,
+    message: string,
+    user: User
 }
 
 export interface User {
