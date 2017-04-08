26b3dfe8c0ed6e18b410ce0f4142756903154eee
Antonio Nakic Alfirevic <antonio.nakic@gmail.com>
2017-04-08 17:48:51 +0200

    Starting client side login functionality

--- Jogging/ClientApp/app/login.service.ts
+++ Jogging/ClientApp/app/login.service.ts
@@ -1,12 +1,28 @@
 ﻿import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs'
 
 @Injectable()
 export class LoginService {
+
+    public redirectUrl: string;
+
     public user: User;
 
-    constructor() {
-        //temp:
-        this.user = { id: 1, firstName: 'Test', lastName: 'User', email: 'test@user.com' }
+    public initUserFromServer() {
+        this.setUserAndRedirect({ id: 1, firstName: 'Test', lastName: 'User', email: 'test@user.com', canCrudAllEntries: false, canCrudUsers: false });
+    }
+
+    private setUserAndRedirect(user: User) {
+        this.user = user;
+        if (user)
+            this.router.navigateByUrl(this.redirectUrl);
+    }
+
+    constructor(private http: Http, private router: Router) {
+        this.redirectUrl = '/entries';
+        this.initUserFromServer();
     }
 }
 
@@ -15,4 +31,6 @@ export interface User {
     firstName: string,
     lastName: string,
     email: string,
+    canCrudUsers: boolean,
+    canCrudAllEntries: boolean
 }
